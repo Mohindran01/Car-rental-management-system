267034314fb1aab05c989686e15d3d6bb2587592
Mohindran01 <61914497+Mohindran01@users.noreply.github.com>
2020-12-16 16:11:35 +0530

    Add files via upload

---
+++ admin/indexx.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>Admin Home</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+</head>
+<body>
+<!-- Header -->
+<div id="header">
+	<div class="shell">
+		<?php
+			include 'menu.php';
+		?>
+		
+	</div>
+</div>
+<!-- End Header -->
+
+<!-- Container -->
+<div id="container">
+	<div class="shell">
+		
+		<!-- Small Nav -->
+		<div class="small-nav">
+			<a href="#">Dashboard</a>
+			<span>&gt;</span>
+			Current Articles
+		</div>
+		<br />
+		<!-- Main -->
+		<div id="main">
+			<div class="cl">&nbsp;</div>
+			
+			<!-- Content -->
+			<div id="content">
+				
+				
+				
+				<!-- Box -->
+				<div class="box">
+					<!-- Box Head -->
+					<div class="box-head">
+						<h2>Add New Article</h2>
+					</div>
+					<!-- End Box Head -->
+					
+					<form action="" method="post">
+						
+						<!-- Form -->
+						<div class="form">
+								<p>
+									<span class="req">max 100 symbols</span>
+									<label>Article Title <span>(Required Field)</span></label>
+									<input type="text" class="field size1" />
+								</p>	
+								<p class="inline-field">
+									<label>Date</label>
+									<select class="field size2">
+										<option value="">23</option>
+									</select>
+									<select class="field size3">
+										<option value="">July</option>
+									</select>
+									<select class="field size3">
+										<option value="">2009</option>
+									</select>
+								</p>
+								
+								<p>
+									<span class="req">max 100 symbols</span>
+									<label>Content <span>(Required Field)</span></label>
+									<textarea class="field size1" rows="10" cols="30"></textarea>
+								</p>	
+							
+						</div>
+						<!-- End Form -->
+						
+						<!-- Form Buttons -->
+						<div class="buttons">
+							<input type="button" class="button" value="preview" />
+							<input type="submit" class="button" value="submit" />
+						</div>
+						<!-- End Form Buttons -->
+					</form>
+				</div>
+				<!-- End Box -->
+
+			</div>
+			<!-- End Content -->
+			
+			
+						
+					
+				<!-- End Box -->
+			<
+			<!-- End Sidebar -->
+			
+			<div class="cl">&nbsp;</div>			
+		</div>
+		<!-- Main -->
+	</div>
+</div>
+<!-- End Container -->
+
+
+	
+</body>
+</html>
